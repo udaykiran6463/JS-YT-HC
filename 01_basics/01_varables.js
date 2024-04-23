e94aa87d8255de9ff9aecae7b7530a51ceff77ca
Uday Kiran Pillalamarri <139199158+udaykiran6463@users.noreply.github.com>
2024-04-23 13:18:31 +0000

    datatypes

---
+++ 01_basics/01_varables.js
@@ -0,0 +1,8 @@
+const accountId = 200716640;
+let accountEmail = "udaykiran6463@gmail.com"
+var accountPassword = "uday@123"
+accountCity = "Hyderabad"
+
+
+console.table([accountEmail,accountId, accountPassword,accountCity])
+console.table([typeof(accountEmail),typeof(accountId),typeof(accountPassword),typeof(accountCity)])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //use new version of the javascript
+// alert("hello") // we are using node.js not browser
+
+console.log(3 + 
+3)
+
+console.log("Uday Kiran")
+
+let name = "uday" // string
+let age = 21;     //number
+let isLoggedIn = true //boolean
+
+
+// number - 2 to power 53
+// bigint
+// string
+// boolean
+// null - stand-along value - representation of the empty value
+// undefined - value is not defined 
+// symbol => unique - to find the uniqueness of the components
+
+
+// object
