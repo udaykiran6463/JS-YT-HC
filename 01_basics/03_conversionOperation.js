c3954d9d81be916b8b750e31a237dbb676442bf5
Uday Kiran Pillalamarri <139199158+udaykiran6463@users.noreply.github.com>
2024-04-23 13:32:00 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = 32
+console.log(typeof score)
+
+let score1 = "33A"
+console.log(typeof score1, score1)
+let valueInNumeber = Number(score1)
+console.log(typeof valueInNumeber, valueInNumeber)
+
+
+//"33" => 33 => type is Number
+// "33a" => NaN => type is also number
+// true => 1 => type is number
+// false => 0 => type is number
+
+console.log(String(false))
+console.log(String(true))
