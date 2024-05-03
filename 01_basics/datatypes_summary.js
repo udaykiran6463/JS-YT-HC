83b96f028e088142069365c968638733408a997e
Uday Kiran Pillalamarri <139199158+udaykiran6463@users.noreply.github.com>
2024-05-03 03:56:23 +0000

    datatypes_summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,10 @@
+const id1 = Symbol('abc')
+const id2 = Symbol('abc')
+console.log(id1)
+console.log(id2)
+console.log(id1 == id2)
+
+
+const myFunction = function(){
+    console.log("Hello World")
+}
